6bb2bc9a5870a139ae1441224d8c4c35da5d1ae0
석훈 이 <nolru7@naver.com>
2019-01-09 10:36:44 +0900

    ip주소

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Web0108
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5000");
     }
 }
